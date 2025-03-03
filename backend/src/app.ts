602efc68a758a4667e5c41bbec73fd4d2613139b
Trần Trung Hiếu <deathclick2309@gmail.com>
2025-03-03 18:06:54 +0700

    Refactor app setup to include UserRoutes and ErrorHandler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,16 @@
-// Importing express, dotenv, cors
+// Importing packages
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
 // Importing db
 import db from './db/index.js';
+
+// Import middlewares
+import ErrorHandler from './middlewares/ErrorHandler.js';
+
+// Routes
+import UserRoutes from './routes/UserRoutes.js';
 
 // Configuring dotenv
 dotenv.config();
@@ -21,16 +27,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/api', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Call API successfully!',
-  });
-});
+app.use('/api', UserRoutes);
+
+// Not found handler
+app.use(ErrorHandler.notFoundError);
+
+// Error handler
+app.use(ErrorHandler.defaultError);
 
 // Server
 const PORT = process.env.PORT || 3000;
 
+// Run if the database has connected
 app.on('ready', () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

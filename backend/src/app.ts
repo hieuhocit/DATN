2b59e529491282dc5ea164c18ad47ba9c25db52e
Trần Trung Hiếu <deathclick2309@gmail.com>
2025-03-05 22:53:15 +0700

    Configure CORS to allow credentials and add cookie parser middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 // Importing db
 import db from './db/index.js';
@@ -25,7 +26,13 @@ const app = express();
 db.connect(app);
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://localhost:5173',
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

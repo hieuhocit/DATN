d418fdb46567b113f14f953c76eb84bdb0ce8542
Trần Trung Hiếu <deathclick2309@gmail.com>
2025-03-05 22:53:07 +0700

    Add login route with validation middleware to AuthRoutes

--- backend/src/routes/AuthRoutes.ts
+++ backend/src/routes/AuthRoutes.ts
@@ -5,12 +5,16 @@ import { Router } from 'express';
 import AuthController from '../controllers/AuthController.js';
 
 // Validate
-import { validateRegister } from '../middlewares/validationMiddleware.js';
+import {
+  validateRegister,
+  validateLogin,
+} from '../middlewares/validationMiddleware.js';
 
 // Create router
 const AuthRoutes = Router();
 
 // Routes
 AuthRoutes.post('/register', validateRegister, AuthController.register);
+AuthRoutes.post('/login', validateLogin, AuthController.login);
 
 export default AuthRoutes;

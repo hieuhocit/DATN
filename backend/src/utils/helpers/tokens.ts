aed10142361c7f670ab8b8ef1e4100a83f7e1a53
Trần Trung Hiếu <deathclick2309@gmail.com>
2025-03-06 19:33:47 +0700

    Add token generation and verification functions for access and refresh tokens

--- backend/src/utils/helpers/tokens.ts
+++ backend/src/utils/helpers/tokens.ts
@@ -2,10 +2,24 @@ import jwt, { SignOptions } from 'jsonwebtoken';
 
 export const generateToken = (
   payload: string | Buffer | object,
-  secret: jwt.Secret | jwt.PrivateKey,
+  type: 'ACCESS' | 'REFRESH',
   expiresIn: SignOptions['expiresIn']
 ) => {
+  const secret =
+    type === 'ACCESS'
+      ? (process.env.ACCESS_TOKEN_SECRET as string)
+      : (process.env.REFRESH_TOKEN_SECRET as string);
+
   return jwt.sign(payload, secret, {
     expiresIn: expiresIn,
   });
+};
+
+export const verifyToken = (token: string, type: 'ACCESS' | 'REFRESH') => {
+  const secret =
+    type === 'ACCESS'
+      ? (process.env.ACCESS_TOKEN_SECRET as string)
+      : (process.env.REFRESH_TOKEN_SECRET as string);
+
+  return jwt.verify(token, secret);
 };

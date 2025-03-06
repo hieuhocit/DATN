f3e2bfe4fb053d002d1180b351a4964a3cf36e86
Trần Trung Hiếu <deathclick2309@gmail.com>
2025-03-06 19:40:49 +0700

    Add forgot password and change password functionality to AuthController

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -10,6 +10,9 @@ import serverResponse from '../utils/helpers/reponses.js';
 // Messages
 import messages from '../configs/messagesConfig.js';
 
+// Types
+import { RequestWithUser } from '../types/types.js';
+
 const AuthController = {
   register: async (req: Request, res: Response, next: NextFunction) => {
     const { email, name, password } = req.body;
@@ -62,6 +65,79 @@ const AuthController = {
             message: 'Login successfully!',
           },
           user
+        )
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+  forgotPassword: async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.body;
+    try {
+      await AuthService.sendResetCode(email);
+
+      res.status(messages.OK.statusCode).json(
+        serverResponse.createSuccess({
+          ...messages.OK,
+          message: 'Reset code has been sent to your email!',
+        })
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+  verifyResetCode: async (req: Request, res: Response, next: NextFunction) => {
+    const { email, resetCode } = req.body;
+
+    try {
+      const result = await AuthService.verifyResetCode(email, resetCode);
+
+      res.status(messages.OK.statusCode).json(
+        serverResponse.createSuccess(
+          {
+            ...messages.OK,
+            message: 'Reset code verified!',
+          },
+          { resetToken: result }
+        )
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+  resetPassword: async (req: Request, res: Response, next: NextFunction) => {
+    const { resetToken, password } = req.body;
+
+    try {
+      await AuthService.resetPassword(resetToken, password);
+
+      res.status(messages.OK.statusCode).json(
+        serverResponse.createSuccess({
+          ...messages.OK,
+          message: 'Password has been reset!',
+        })
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+  changePassword: async (req: Request, res: Response, next: NextFunction) => {
+    const user = (req as RequestWithUser).user;
+    const { oldPassword, newPassword } = req.body;
+    try {
+      const data = await AuthService.changePassword(
+        user.email,
+        oldPassword,
+        newPassword
+      );
+
+      res.status(messages.OK.statusCode).json(
+        serverResponse.createSuccess(
+          {
+            ...messages.OK,
+            message: 'Password has been changed!',
+          },
+          data
         )
       );
     } catch (error) {

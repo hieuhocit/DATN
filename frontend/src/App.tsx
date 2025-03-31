0a41f449900b5f2f2939fff089996b0d33bac0d8
Trần Trung Hiếu <deathclick2309@gmail.com>
2025-03-31 14:01:49 +0700

    Integrate React Router and React Query into the main App component for improved routing and data fetching

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+// React-router
+import { RouterProvider } from 'react-router/dom';
+
+// router
+import router from '@routes/routes';
+
+// react query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Query Client
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

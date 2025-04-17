018ca11dd424c2d71d1a0e3108a4f70402f6675a
Trần Trung Hiếu <trantrunghieu2393@gmail.com>
2025-04-17 11:08:13 +0700

    feat: Reintroduce DesktopHeader component with complete functionality

---
+++ frontend/src/components/header/desktop/DesktopHeader.tsx
@@ -0,0 +1,62 @@
+// MUI
+import Box from '@mui/material/Box';
+import { Stack } from '@mui/material';
+
+// React router
+import { Link } from 'react-router-dom';
+
+// Components
+import Dropdown from './Dropdown';
+import Image from '@components/common/Image';
+import SearchBar from '@components/common/SearchBar';
+import ToggleThemeMode from '@/components/common/ToggleThemeMode';
+import Cart from './Cart';
+import Notification from './Notification';
+import Profile from './Profile';
+
+export default function DesktopHeader() {
+  return (
+    <Stack direction={'row'} alignItems='center' gap={8} sx={{ width: '100%' }}>
+      <Stack direction={'row'} spacing={3} alignItems='center'>
+        {/* Logo */}
+        <Link to={'/'}>
+          <Box
+            sx={{
+              position: 'relative',
+              width: '120px',
+              aspectRatio: '2/1',
+              mt: '-5px !important',
+            }}
+          >
+            <Image
+              style={{ objectFit: 'contain' }}
+              src='/icons/dark-logo.svg'
+              alt='Logo'
+              fill={true}
+            />
+          </Box>
+        </Link>
+
+        {/* Categories */}
+        <Dropdown />
+      </Stack>
+
+      {/* Search */}
+      <SearchBar sx={{ flexGrow: 1 }} />
+
+      <Stack direction={'row'} gap={1} alignItems={'center'}>
+        {/* Them mode */}
+        <ToggleThemeMode />
+
+        {/* Cart */}
+        <Cart />
+
+        {/* Notifications */}
+        <Notification />
+
+        {/* Profile */}
+        <Profile />
+      </Stack>
+    </Stack>
+  );
+}

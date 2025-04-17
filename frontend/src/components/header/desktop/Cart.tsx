8279e1d9bddd26aefb04584bacf1437f43595be3
Trần Trung Hiếu <trantrunghieu2393@gmail.com>
2025-04-17 11:09:19 +0700

    feat: Implement Cart component with shopping cart functionality and item display

---
+++ frontend/src/components/header/desktop/Cart.tsx
@@ -0,0 +1,110 @@
+// Icons
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+// React
+import { useState } from 'react';
+
+// Components
+import Image from '@/components/common/Image';
+
+// MUI
+import Box from '@mui/material/Box';
+import Badge from '@mui/material/Badge';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Stack, Tooltip, Typography } from '@mui/material';
+import { OneLineTypography, TwoLineTypography } from '@/components/typography';
+
+export default function Cart() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <Box>
+        <Tooltip title='Giỏ hàng'>
+          <IconButton onClick={handleClick} size='large' color='inherit'>
+            <Badge badgeContent={2} color='error'>
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        </Tooltip>
+        <Menu
+          MenuListProps={{
+            sx: { padding: 0 },
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <CartItem />
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <CartItem />
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <CartItem />
+          </MenuItem>
+        </Menu>
+      </Box>
+    </>
+  );
+}
+
+function CartItem() {
+  return (
+    <>
+      <Stack direction={'row'} gap={1} alignItems={'flex-start'}>
+        <Box
+          sx={{
+            width: '70px',
+            position: 'relative',
+            aspectRatio: '1/1',
+            borderRadius: 1,
+            overflow: 'hidden',
+          }}
+        >
+          <Image src='/images/image-placeholder.png' fill />
+        </Box>
+        <Stack direction={'column'} gap={'6px'}>
+          <TwoLineTypography sx={{ fontSize: '0.9rem' }} variant='body1'>
+            Master HTML CSS với khóa học HTML CSS Tips
+          </TwoLineTypography>
+
+          <Stack>
+            <OneLineTypography variant='body1'>
+              Stephane Maarek | AWS Certified Cloud Practitioner,Solutions
+              Architect,Developer
+            </OneLineTypography>
+            <Stack direction={'row'} gap={1} alignItems={'center'}>
+              <Typography
+                sx={{ fontSize: '0.9rem' }}
+                variant='body1'
+                fontWeight={600}
+              >
+                ₫399,000
+              </Typography>
+              <Typography
+                sx={{ textDecoration: 'line-through' }}
+                variant='body1'
+                fontSize={'0.75rem'}
+              >
+                ₫2,329,000
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </>
+  );
+}

1c6e080550fdf2e0abd9950e0806e06780afb646
Trần Trung Hiếu <trantrunghieu2393@gmail.com>
2025-04-17 11:07:29 +0700

    feat: Refactor theme palette structure for improved clarity and organization

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -1,16 +1,13 @@
 // MUI theme configuration
 import { createTheme, responsiveFontSizes } from '@mui/material';
-import { palette } from './palette';
+import { lightPalette, darkPalette } from './palette';
 import { components } from './components';
 import { typography } from './typography';
 
 export const getTheme = (mode: 'light' | 'dark') => {
   return responsiveFontSizes(
     createTheme({
-      palette: {
-        mode: mode,
-        ...palette,
-      },
+      palette: mode === 'light' ? lightPalette : darkPalette,
       typography: typography,
       components: components,
     })
--- frontend/src/theme/palette.ts
+++ frontend/src/theme/palette.ts
@@ -4,9 +4,21 @@ import type { PaletteOptions } from '@mui/material';
 // Colors
 import { common } from '@mui/material/colors';
 
-export const palette: PaletteOptions = {
+export const lightPalette: PaletteOptions = {
+  mode: 'light',
   primary: {
     main: common.white,
     contrastText: common.black,
+  },
+  action: {
+    hover: 'rgba(0, 0, 0, 0.15) !important',
+  },
+};
+
+export const darkPalette: PaletteOptions = {
+  mode: 'dark',
+  background: {
+    paper: '#212529',
+    default: '#000',
   },
 };

efb6ef59226c5aa0d7731b51dc5be28bf90feba2
Trần Trung Hiếu <trantrunghieu2393@gmail.com>
2025-04-19 23:21:30 +0700

    feat: Add account reducer to Redux store configuration

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // Reduces
 import { themeReducer } from '@features/theme/index';
+import { accountReducer } from '@features/account/index';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
+    account: accountReducer,
   },
 });
 

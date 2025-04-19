214e317e061ac1c948c2d42c1a2de1f912cd4983
Trần Trung Hiếu <trantrunghieu2393@gmail.com>
2025-04-19 23:21:01 +0700

    feat: Refactor authService to include login, register, and logout functions with proper response types

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,60 @@
 import axios from '@/configs/axiosConfig';
 
-export const verifyToken = async () => {
-  const data = await axios.post('/verify-token');
-  return data;
+// Types
+import { User } from '@/types';
+
+interface CommonResponse {
+  statusCode: number;
+  statusText: string;
+  message: string;
+}
+
+interface LoginResponse extends CommonResponse {
+  data?: User;
+  errors?: Record<string, { field: string; message: string }>;
+}
+
+interface RegisterResponse extends CommonResponse {
+  data?: User;
+  errors?: Record<string, { field: string; message: string }>;
+}
+
+interface VerifyTokenResponse extends CommonResponse {
+  data?: User;
+}
+
+export const login = async (data: object): Promise<LoginResponse> => {
+  try {
+    const result = (await axios.post('/login', data)) as LoginResponse;
+    return result;
+  } catch (error) {
+    return error as LoginResponse;
+  }
+};
+
+export const register = async (data: object) => {
+  try {
+    const result = (await axios.post('/register', data)) as RegisterResponse;
+    return result;
+  } catch (error) {
+    return error as RegisterResponse;
+  }
+};
+
+export const verifyToken = async (): Promise<VerifyTokenResponse> => {
+  try {
+    const data = (await axios.post('/verify-token')) as VerifyTokenResponse;
+    return data;
+  } catch (error) {
+    return error as VerifyTokenResponse;
+  }
+};
+
+export const logout = async () => {
+  try {
+    const result = (await axios.post('/logout')) as CommonResponse;
+    return result;
+  } catch (error) {
+    return error as CommonResponse;
+  }
 };
